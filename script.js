753c2c94599afda911314e126de3d650da1a7e5e
onekmath <34621673+onekmath@users.noreply.github.com>
2023-04-01 09:58:59 +0700

    - add input submit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const board = document.getElementsByClassName('board')[0];
 
-board.textContent = "Welcome to my World";
+board.textContent = "Phía trước là đường!";
+
+document.getElementById("button").onclick = function(){
+    var userName = document.getElementById("textbox").value;
+    board.textContent = userName + " đã từng qua đây.";
+    
+}

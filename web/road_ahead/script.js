61ae2392fda496202a9cd57eab793fa3d250ad63
onekmath <34621673+onekmath@users.noreply.github.com>
2023-09-04 09:16:21 +0700

    -reset

--- web/road_ahead/script.js
+++
@@ -1,9 +0,0 @@
-const board = document.getElementsByClassName('board')[0];
-board.textContent = "Phía trước là đường!";
-
-document.getElementById("button").onclick = function(){
-    var userName = document.getElementById("textbox").value;
-    board.style.backgroundColor = "#fff";
-    board.textContent = userName + " đã từng qua đây.";
-
-}

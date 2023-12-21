03b7835d4b7b3f82963a86b691a4efabc4f5b05f
onekmath <34621673+onekmath@users.noreply.github.com>
2023-12-21 15:57:40 +0700

    -- add cmsn vk page

---
+++ web-demo/cmsn-vk-2023/script.js
@@ -0,0 +1,30 @@
+const click = document.querySelector('.click');
+const giftBox = document.querySelector('.gift-box');
+const shadow = document.querySelector('.shadow');
+const giftContainer = document.querySelector('.gift-container');
+const text = document.querySelector('.text')
+const man = document.querySelector('.man')
+
+
+click.addEventListener('click', () => {
+    if (click.className == "click") {
+        click.classList.add('active');
+        giftBox.classList.add('active');
+        shadow.classList.add('active');
+        giftContainer.classList.add('active');
+        text.classList.add('active');
+        text.classList.remove('active2');
+        man.classList.add('active');
+        man.classList.remove('active2');
+    }
+    else{
+        click.classList.remove('active');
+        giftBox.classList.remove('active');
+        shadow.classList.remove('active');
+        giftContainer.classList.remove('active');
+        text.classList.remove('active');
+        text.classList.add('active2');
+        man.classList.remove('active');
+        man.classList.add('active2');
+    }
+})
